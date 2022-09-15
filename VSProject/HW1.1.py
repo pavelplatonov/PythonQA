3c44b35bd8334e4dd6219da6b77ffb1ed4639c5a
Павел <Pavelplaton@gmail.com>
2022-09-16 01:16:56 +0300

    переделал структуру

---
+++ VSProject/HW1.1.py
@@ -0,0 +1,7 @@
+message = 'Пароль принят'
+message_error = 'Пароль не принят'
+password = input('Введите пароль: ')
+password_second = input('Подтверите пароль: ')
+if password == password_second:
+    print(message)
+else: print(message_error)
---
+++ VSProject/HW1.py
@@ -0,0 +1,13 @@
+vote = input('Write your geometry (Cube, Trio, Circle):')
+line = '*' * 9
+stena = '*' + ' ' * 7 + '*'
+cube = (line + '\n' + stena + '\n' + stena + '\n' + line)
+error = 'This is not Cube, Trio or Circle'
+
+if vote == 'Cube':
+    print(cube)
+elif vote == 'Trio': #треугольник не доделан и брошен на произвол
+    print(line)
+elif vote == 'Circle': #круг не доделан и брошен на произвол
+    print(stena)
+else: print(error)
