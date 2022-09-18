707812abbba38e3f36c586bc7ac3d77cd8d8923b
Павел <Pavelplaton@gmail.com>
2022-09-18 05:22:23 +0300

    save with black and flake8

--- HW/HW1.1.py
+++ HW/HW1.1.py
@@ -1,10 +1,9 @@
-message = 'Пароль принят'
-message_error = 'Пароль не принят'
-password = input('Введите пароль: ')
-password_second = input('Подтверите пароль: ')
+message = "Пароль принят"
+message_error = "Пароль не принят"
+password = input("Введите пароль: ")
+password_second = input("Подтверите пароль: ")
 if password == password_second:
     print(message)
-else: print(message_error)
-    
-    
-    #test
+else:
+    print(message_error)
+# test
--- HW/HW1.py
+++ HW/HW1.py
@@ -1,13 +1,14 @@
-vote = input('Write your geometry (Cube, Trio, Circle):')
-line = '*' * 9
-stena = '*' + ' ' * 7 + '*'
-cube = (line + '\n' + stena + '\n' + stena + '\n' + line)
-error = 'This is not Cube, Trio or Circle'
+vote = input("Write your geometry (Cube, Trio, Circle):")
+line = "*" * 9
+stena = "*" + " " * 7 + "*"
+cube = line + "\n" + stena + "\n" + stena + "\n" + line
+error = "This is not Cube, Trio or Circle"
 
-if vote == 'Cube':
+if vote == "Cube":
     print(cube)
-elif vote == 'Trio': #треугольник не доделан и брошен на произвол
+elif vote == "Trio":  # треугольник не доделан и брошен на произвол
     print(line)
-elif vote == 'Circle': #круг не доделан и брошен на произвол
+elif vote == "Circle":  # круг не доделан и брошен на произвол
     print(stena)
-else: print(error)
+else:
+    print(error)
