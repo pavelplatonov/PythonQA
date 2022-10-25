60571c5c46653230b25a11d027504d2f4302a8e3
Павел <Pavelplaton@gmail.com>
2022-10-26 02:35:05 +0300

    hw5

--- HW/HW5.py
+++ HW/HW5.py
@@ -3,6 +3,84 @@
 #       -   как минимум один атрибут должен быть с уровнем доступа private.
 #           Соответственно, для получания значений этого атрибута
 #           нужно использовать методы get и set
+from unicodedata import name
 
+
+class Employee:  # класс сотрудники
+    """Базовый класс для всех сотрудников"""
+
+    emp_count = 0  # счётчик сотрудников
+
+    def __init__(self, name, salary, hours):  # функция добавления нового сотрудника
+        self.name = name  # аргумент класса имя
+        self.salary = salary  # аргумент класса зарплата
+        self.hours = hours  # аргумент класса с параметром часы
+        Employee.emp_count += 1  # команда на увеличение счётчика сотрудников
+
+    def display_count(
+        self,
+    ):  # метод класса с выводом текста в определенном формате
+        # и счётчиком сотрудников
+        print("Всего сотрудников: %d" % Employee.emp_count)
+
+    def display_employee(self):
+        print(
+            "Имя: {}. Зарплата: {}. Количество рабочих часов: {}".format(
+                self.name, self.salary, self.hours
+            )
+        )
+
+
+# Это создаст первый объект класса Employee
+emp1 = Employee("Андрей", 2000, 12)
+# Это создаст второй объект класса Employee
+emp2 = Employee("Мария", 5000, 8)
+# Это создаст третий объект класса Employee
+emp3 = Employee("Павел", 8000, 6)
+# вывод параметров объекта emp1 и emp2 при помощи метода display_employee
+emp1.display_employee()
+emp2.display_employee()
+emp3.display_employee()
+print("Всего сотрудников: %d" % Employee.emp_count)
+
+
+class tovar:  # Создаём класс товар
+    """Базовый класс для всех товаров"""
+
+    tovar_count = 0
+
+    def __init__(self, artikul, name, ves, sklad):
+        self.artikul = artikul
+        self.__name = name
+        self.ves = ves
+        self.sklad = sklad
+        tovar.tovar_count += 1
+
+    def display_count(
+        self,
+    ):
+        print("На складе осталось %d товаров" % tovar.tovar_count)
+
+    def display_pozicia(self):
+        print(
+            "Позиция с артикулом {}, наименованием {}, весом {}, на складе {}".format(
+                self.artikul, self.__name, self.ves, self.sklad
+            )
+        )
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self):
+        self.__name = name
+
+
+tort_praga = tovar("00112", "Praga", "1200gr", "Kond.ceh")
+tort_mars = tovar("00113", "Marsel", "700gr", "Kond.ceh")
+
+tort_mars.display_pozicia()
+tort_praga.display_pozicia()
+
+print("На складе имеется %d товара" % tovar.tovar_count)
 # 5.2.  Cоздайте репозиторий на Github и отправте файл с домашним заданием
 #       в этот удаленный репозиторий
---
+++ HW/HW6.py
@@ -0,0 +1,8 @@
+# 7.1. Найдите кпопку c текстом "Gold".
+# Попробуйте подобрать как минимум 2 разных XPATH и/или CSS-селекторов
+# http://suninjuly.github.io/xpath_examples
+
+
+# 7.2. Найдите элемент с текстом "Lenin cat".
+# Чем больше разных XPATH и/или CSS-селекторов сможете подобрать, тем лучше
+# http://suninjuly.github.io/cats.html
