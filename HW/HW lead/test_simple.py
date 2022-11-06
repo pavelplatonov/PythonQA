3b94af1408ccc01b5488ffabb9eba25a0893248f
Павел <Pavelplaton@gmail.com>
2022-11-06 21:17:22 +0300

    lesson6 and hw5 and hw lead

---
+++ HW/HW lead/test_simple.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+def test_simple():
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+    driver.get("http://selenium.dev")
+
+    assert driver.title == "Selenium"
+
+    driver.quit()
---
+++ HW/lesson6.1/test_login_form.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.saucedemo.com/")
+
+
+def test_title():
+
+    title_from_site = driver.title
+    assert title_from_site == "Swag Labs"
+
+
+def test_login_form():
+    time.sleep(1)
+    # driver.find_element((By.CSS_SELECTOR, 'input#user-name'))
+    user_name = driver.find_element(By.ID, "user-name")
+    user_name.send_keys("standard_user")
+    time.sleep(1)
+
+    # password = driver.find_element(By.XPATH, "//input[contains(@placeholder, \'Password\')]")
+    password = driver.find_element(By.ID, "password")
+    password.send_keys("secret_sauce")
+    time.sleep(1)
+
+    # button_login = driver.find_element(By.XPATH, "//input[@name='login_button']")
+    button_login = driver.find_element(By.NAME, "login-button")
+    button_login.click()
+    time.sleep(1)
+
+    assert (
+        driver.current_url == "https://www.saucedemo.com/inventory.html"
+    ), "We reached another site!!!"
+
+    driver.quit
